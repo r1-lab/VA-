5b2381f0c7957509099a0b467621f48571a16151
chanujki <uca_copyright_zoon@yahoo.com>
2025-05-29 11:19:47 +0600

    Update friend.js

--- scripts/commands/friend.js
+++ scripts/commands/friend.js
@@ -1,71 +1,91 @@
-const axios = require('axios');
-const jimp = require("jimp");
-const fs = require("fs");
-
 module.exports.config = {
-  name: "friend",
-  version: "1.0.0",
-  permssion: 0,
-  credits: "Mohammad Nayan",
-  description: "friends photo frame",
-  category: "friends",
-  prefix: true,
-  cooldowns: 2,
+    name: "friends",
+    version: "1.0.0",
+    permission: 3,
+    credits: "ryuko",
+    description: "list friends",
+    prefix: true,
+    category: "operator",
+    usages: "friends",
+    cooldowns: 5,
 };
-
-module.exports.run = async function({ api, event, args, Users, threadID, senderID, messageID }) {
-  if (module.exports.config.credits !== "Mohammad Nayan") {
-    return api.sendMessage(
-      "Warning: You have changed the credits (Mohammad Nayan). Please replace it with the original one.",
-      threadID,
-      messageID
-    );
+ 
+module.exports.handleReply = async function ({ api, args, Users, handleReply, event, Threads, getText }) {
+  const { threadID, messageID } = event;
+  if (parseInt(event.senderID) !== parseInt(handleReply.author)) return;
+ 
+  switch (handleReply.type) {
+    case "reply":
+      {
+        var msg ="" , name, urlUser, uidUser;
+        var arrnum = event.body.split(" ");
+        var nums = arrnum.map(n => parseInt(n));
+        for (let num of nums) {
+          name = handleReply.nameUser[num - 1];
+          urlUser = handleReply.urlUser[num - 1];
+          uidUser = handleReply.uidUser[num - 1];
+ 
+          api.unfriend(uidUser);
+          msg += 'name : ' + name + '\nlink : ' + urlUser + "\n";
+          //console.log(msg);
+        }
+ 
+        api.sendMessage(`successfully deleted from friend list\n\n${msg}`, threadID, () =>
+          api.unsendMessage(handleReply.messageID));
+      }
+      break;
   }
-
-  const uid = senderID;
-  const info = args.join(" ");
-  const userName = await Users.getNameUser(uid);
-
-  if (!info) {
-    return api.sendMessage(`${userName}, Please tag your 2 friends.`, threadID);
-  }
-
-  const mentionedIDs = Object.keys(event.mentions);
-
-  if (mentionedIDs.length < 2) {
-    return api.sendMessage("Please mention two friends to use this command.", threadID);
-  }
-
-  const id1 = mentionedIDs[0];
-  const id2 = mentionedIDs[1];
-
-  const name1 = await Users.getNameUser(uid); 
-  const name2 = await Users.getNameUser(id1); 
-  const name3 = await Users.getNameUser(id2); 
-
-  api.sendMessage("Processing your image, please wait...", threadID, (err, info) => {
-    setTimeout(() => api.unsendMessage(info.messageID), 5000);
-  });
-
+};
+ 
+ 
+module.exports.run = async function ({ event, api, args }) {
+  const { threadID, messageID, senderID } = event;
+  //var unfriend =  await api.unfriend();
   try {
-    const apiUrl = `https://raw.githubusercontent.com/MOHAMMAD-NAYAN/Nayan/main/api.json`;
-    const apiResponse = await axios.get(apiUrl);
-    const apiData = apiResponse.data.api;
-    const imgUrl = `${apiData}/nayan/friend?uid=${id1}&uid2=${uid}&uid3=${id2}`;
-
-    const response = await axios.get(imgUrl, { responseType: 'arraybuffer' });
-    const image = await jimp.read(response.data);
-    const outputPath = `./fbcoverv2_${uid}.png`;
-
-    await image.writeAsync(outputPath);
-
-    const attachment = fs.createReadStream(outputPath);
-    api.sendMessage({
-      body: `[💏] FRIEND 1: ${name1}\n[💏] FRIEND 2: ${name2}\n[💏] FRIEND 3: ${name3}`,
-      attachment
-    }, threadID, () => fs.unlinkSync(outputPath)); 
-  } catch (error) {
-    console.error(error);
-    api.sendMessage("An error occurred while generating the image.", threadID);
+    var listFriend = [];
+    var dataFriend = await api.getFriendsList();
+    var countFr = dataFriend.length;
+ 
+    for (var friends of dataFriend) {
+      listFriend.push({
+        name: friends.fullName || "Chưa đặt tên",
+        uid: friends.userID,
+        gender: friends.gender,
+        vanity: friends.vanity,
+        profileUrl: friends.profileUrl
+      });
+    }
+    var nameUser = [], urlUser = [], uidUser = [];
+    var page = 1;
+    page = parseInt(args[0]) || 1;
+    page < -1 ? page = 1 : "";
+    var limit = 10;
+    var msg = `you have ${countFr} friend/s\n\n`;
+    var numPage = Math.ceil(listFriend.length / limit);
+ 
+    for (var i = limit * (page - 1); i < limit * (page - 1) + limit; i++) {
+      if (i >= listFriend.length) break;
+      let infoFriend = listFriend[i];
+      msg += `${i + 1}. ${infoFriend.name}\nid : ${infoFriend.uid}\ngender : ${infoFriend.gender}\nvanity : ${infoFriend.vanity}\nfacebook link : ${infoFriend.profileUrl}\n\n`;
+      nameUser.push(infoFriend.name);
+      urlUser.push(infoFriend.profileUrl);
+      uidUser.push(infoFriend.uid);
+    }
+    msg += `page ${page}/${numPage}\nuse ${global.config.PREFIX}friend number/all\n\n`;
+ 
+    return api.sendMessage(msg + `reply the number of your friend if you want to remove it you can also unfriend multiple using (1, 2, 3 etc.)`, event.threadID, (e, data) =>
+      global.client.handleReply.push({
+        name: this.config.name,
+        author: event.senderID,
+        messageID: data.messageID,
+        nameUser,
+        urlUser,
+        uidUser,
+        type: 'reply'
+      })
+    )
   }
-};
+  catch (e) {
+    return console.log(e)
+  }
+}
